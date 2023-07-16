e9cdd24ada9ee48ab4faf594a56225ca8c346436
Even Lin <nameeven@hotmail.com>
2023-07-16 17:29:43 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+let displayValue = "";
+let operator = "";
+let operand1 = null;
+let operand2 = null;
+let result = null;
+
+function appendNumber(number) {
+  displayValue += number;
+  updateDisplay();
+}
+
+function appendDecimal(decimal) {
+  if (!displayValue.includes(decimal)) {
+    displayValue += decimal;
+    updateDisplay();
+  }
+}
+
+function setOperator(op) {
+  operator = op;
+  operand1 = parseFloat(displayValue);
+  displayValue = "";
+}
+
+function calculate() {
+  operand2 = parseFloat(displayValue);
+  if (operator === "+") {
+    result = operand1 + operand2;
+  } else if (operator === "-") {
+    result = operand1 - operand2;
+  } else if (operator === "*") {
+    result = operand1 * operand2;
+  } else if (operator === "/") {
+    result = operand1 / operand2;
+  }
+
+  //displayValue = result.toString();
+  displayValue= result.toLocaleString();//千分位
+  updateDisplay();
+
+  // Reset variables for the next calculation
+  operator = "";
+  operand1 = null;
+  operand2 = null;
+  result = null;
+}
+
+function updateDisplay() {
+  document.getElementById("display").value = displayValue;
+}
+
+function handleKeyboardInput(event) {
+  const { key } = event;
+  if (/\d/.test(key)) {
+    appendNumber(key);
+  } else if (key === ".") {
+    appendDecimal(key);
+  } else if (/[+\-*/]/.test(key)) {
+    setOperator(key);
+  } else if (key === "=" || key === "Enter") {
+    calculate();
+  } else if (key === "Backspace") {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+  } else if (key === "Escape") {
+    displayValue = "";
+    updateDisplay();
+  }
+}
+
+document.addEventListener("keydown", handleKeyboardInput);
